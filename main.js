5dba964ba375aac60762ae65055f9bed956e101a
Артем Комаров <komarov.ar@office.lan>
2024-12-19 13:21:32 +0100

    feat: добавляет скролл по клику на пагинацию

--- main.js
+++ main.js
@@ -1,57 +1,84 @@
 import "./style.css";
 import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-
     const animateSection = () => {
       let mm = gsap.matchMedia();
-      let ctx = null
+      let ctx = null;
 
       mm.add(
         {
-          isDesktop: '(min-width: 1024px)',
-          isMobile: '(max-width: 1023px)',
+          isDesktop: "(min-width: 1024px)",
+          isMobile: "(max-width: 1023px)",
         },
         (context) => {
-          const { isDesktop, isMobile } = context.conditions
+          const { isDesktop, isMobile } = context.conditions;
 
-          const cards = document.querySelectorAll('.big-card')
+          const cards = document.querySelectorAll(".big-card");
 
           if (isDesktop) {
             ctx = gsap.context(() => {
-              ScrollTrigger.create({
-                trigger: '.anim-container',
-                start: 'top top',
-                end: 'bottom bottom',
-                pin: '.anim-container__pinned-content',
-                pinSpacing: false,
-              })
+              const timeline = gsap.timeline({
+                scrollTrigger: {
+                  trigger: ".anim-container",
+                  start: "top top",
+                  end: "bottom bottom",
+                  pin: ".anim-container__pinned-content",
+                  pinSpacing: false,
+                  scrub: true,
+                },
+              });
 
-              const paginationItems = document.querySelectorAll('.anim-container__pagination-item')
+              const paginationItems = document.querySelectorAll(
+                ".anim-container__pagination-item"
+              );
+
+
+              const getScrollValueByCard = (card) => {
+                const cardRect = card.getBoundingClientRect();
+                const windowHeight = window.innerHeight;
+                const scrollY = window.scrollY || window.pageYOffset;
+                return cardRect.top + scrollY - (windowHeight / 2 - cardRect.height / 2);
+              };
+
               paginationItems.forEach((item, index) => {
-                item.classList.remove('active')
-              })
+                item.classList.remove("active");
+                item.addEventListener("click", () => {
+                  let scrolltoCardValue = getScrollValueByCard(cards[index]);
+                  gsap.to(window, {
+                    scrollTo: { y: scrolltoCardValue, autoKill: false },
+                    overwrite: true,
+                    duration: 1,
+                  });
+                });
+              });
+
               cards.forEach((card, index) => {
-                gsap.from(card, {
+                timeline.from(card, {
                   yPercent: 10,
-                  ease: 'power4',
+                  ease: "power4",
                   scrollTrigger: {
                     trigger: card,
-                    start: 'top 80%',
+                    start: "top 80%",
                     scrub: true,
                     markers: false,
                     id: `card-${index + 1} isDesktop`,
-                    onEnter: () => paginationItems[index].classList.add('active'),
-                    onLeaveBack: () => paginationItems[index].classList.remove('active'),
+                    onEnter: () =>
+                      paginationItems[index].classList.add("active"),
+                    onLeaveBack: () =>
+                      paginationItems[index].classList.remove("active"),
                   },
-                })
-              })
-            })
+                });
+              });
+
+            });
+            // end if desktop
           }
 
           if (isMobile) {
@@ -60,23 +87,23 @@ document.addEventListener(
                 gsap.from(card, {
                   yPercent: 40,
                   opacity: 0,
-                  ease: 'power4',
+                  ease: "power4",
                   scrollTrigger: {
                     trigger: card,
-                    start: 'top 90%',
-                    end: 'top center',
+                    start: "top 90%",
+                    end: "top center",
                     scrub: true,
                     markers: false,
                     id: `${index} - mobile`,
                   },
-                })
-              })
-            })
+                });
+              });
+            });
           }
-        },
-      )
-    }
-    animateSection()
+        }
+      );
+    };
+    animateSection();
   },
   true
 );
